a0b15cd98452146c84ce8b563d74ff46068e7b41
julio <julio.akiyama@gmail.com>
2017-08-18 22:24:59 +0900

    task done

--- pm-proj011/js/script.js
+++ pm-proj011/js/script.js
@@ -1,20 +1,19 @@
 document.querySelector('#result').classList.add('hidden');
-//var height, weight, calculate, result, msg;
-
-document.querySelector('button').addEventListener('click', checkInput);
 
 
 //calculate BMI and show the results
-var calcBMI = function() {
-  var height = document.querySelector('#height').value.replace(/,/g, ".");
-  var weight = document.querySelector('#weight').value.replace(/,/g, ".");
-  var calculate = weight / (height * height);
-  var result = calculate.toFixed(2);
-  var msg = document.querySelector('h1');
-  var p = document.querySelector('p');
+var calcBMI = function () {
+  'use strict';
+  var height, weight, calculate, result, msg, p;
+  height = document.querySelector('#height').value.replace(/,/g, ".");
+  weight = document.querySelector('#weight').value.replace(/,/g, ".");
+  calculate = weight / (height * height);
+  result = calculate.toFixed(2);
+  msg = document.querySelector('h1');
+  p = document.querySelector('p');
   document.querySelector('button').classList.add('hidden');
   document.querySelector('#result').classList.remove('hidden');
-  msg.textContent = result ; 
+  msg.textContent = result;
   
   if (result <= 16) {
     p.textContent = 'Magresa Grave';
@@ -23,49 +22,53 @@ var calcBMI = function() {
   } else if (result > 17 && result <= 18.5) {
     p.textContent = 'Magresa Leve';
   } else if (result > 18.5 && result <= 25) {
-    p.textContent = 'Saudavél'; 
+    p.textContent = 'Saudavél';
   } else if (result > 25 && result <= 30) {
-    p.textContent = 'Sobre Peso'; 
+    p.textContent = 'Sobre Peso';
   } else if (result > 30 && result <= 35) {
-    p.textContent = 'Obeso'; 
+    p.textContent = 'Obeso';
   } else if (result > 35 && result <= 40) {
-    p.textContent = 'Obesidade Severa'; 
+    p.textContent = 'Obesidade Severa';
   } else if (result > 40) {
-    p.textContent = 'Obesidade Mórbida'; 
+    p.textContent = 'Obesidade Mórbida';
   }
 };
 
 
 //checking the inputs for undefined, '', null
 function checkInput() {
-  var inputText = document.getElementsByTagName('input');
+  'use strict';
+  var inputText, letters, i;
+  inputText = document.getElementsByTagName('input');
   //var letters = /^[a-zA-Z]+$/;
-  var letters = /^[a-zA-Z!@#$%\^&*)(+=_-]*$/;
+  letters = /^[azAZ!@#$%\^&*)(+=_]*$/;
   
-  for (var i = 0; i < inputText.length; i++) {
+  for (i = 0; i < inputText.length; i += 1) {
     if ((inputText[i].value === '') || (inputText[i].value <= 0) || (inputText[i].value.match(letters)) || (isNaN(inputText[i].value.replace(/,/g, ".")))) {
       inputText[i].focus();
       
       document.querySelector('button').classList.remove('hidden');
       document.querySelector('#result').classList.add('hidden');
       return false;
-    }  else {
-        calcBMI();
+    } else {
+      calcBMI();
     }
   }
 }
 
 
 //Just saying bye-bye
-document.querySelector('#result').addEventListener('click', function(){
-  var inputHeight = document.querySelector('#height');
-  var inputWeight = document.querySelector('#weight');
+document.querySelector('#result').addEventListener('click', function () {
+  'use strict';
+  var inputHeight, inputWeight;
+  inputHeight = document.querySelector('#height');
+  inputWeight = document.querySelector('#weight');
   
-  if  (inputHeight.value !== ''){
+  if (inputHeight.value !== '') {
     inputHeight.value = 'Playcode';
-  } 
+  }
   
-  if (inputWeight.value !== ''){
+  if (inputWeight.value !== '') {
     inputWeight.value = 'Makers';
   }
   document.querySelector('button').classList.remove('hidden');
@@ -73,7 +76,7 @@ document.querySelector('#result').addEventListener('click', function(){
 });
 
 
-
+document.querySelector('button').addEventListener('click', checkInput);
 
 
 
